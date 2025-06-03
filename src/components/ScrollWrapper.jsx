827e32bcea9e6ac5c0df78c678cb3162ea541958
kevinmspence <kevinmspence@gmail.com>
2025-06-02 22:43:34 -0400

    Switch to standalone mode via next.config.js

--- src/components/ScrollWrapper.jsx
+++ src/components/ScrollWrapper.jsx
@@ -11,20 +11,29 @@ export default function ScrollWrapper({ title, children, size = "default" }) {
         draggable={false}
       />
 
-      {/* Middle */}
-      <div
-        className="bg-repeat-y bg-[length:100%_auto] px-4 sm:px-8"
-        style={{
-          backgroundImage: `url(../${prefix}-middle.png)`,
-        }}
-      >
-        <div className="w-full">
+      {/* Middle – Repeat scroll-middle.png as a tiled img */}
+      <div className="w-full relative">
+        <div className="absolute inset-0 z-0">
+          <div
+            className="w-full"
+            style={{
+              backgroundImage: `url(../${prefix}-middle.png)`,
+              backgroundRepeat: 'repeat-y',
+              backgroundSize: '100% auto',
+            }}
+          >
+            <div className="invisible">.</div>
+          </div>
+        </div>
+
+        {/* Content over background */}
+        <div className="relative z-10 px-4 sm:px-8 pt-6 pb-8">
           {title && (
             <h2 className="text-xl sm:text-2xl font-rock text-yellow-200 tracking-wide text-center mb-6">
               {title}
             </h2>
           )}
-          <div className="w-full">{children}</div>
+          {children}
         </div>
       </div>
 
