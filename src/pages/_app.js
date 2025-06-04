a35433d8cc0ff4b8bf14d419b94c8315bc23f407
kevinmspence <kevinmspence@gmail.com>
2025-06-03 21:57:32 -0400

    Add collection page and fix showdate serialization

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,51 @@
 import "@/styles/globals.css";
 import Head from "next/head";
-import Layout from "@/components/Layout"; // make sure this path is correct
+import Layout from "@/components/Layout";
+import Script from "next/script";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag?.('config', 'G-6G9CLBBL99', {
+        page_path: url,
+      });
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router]);
+
   return (
     <>
       <Head>
         <link rel="icon" href="/fish-favicon.ico" />
       </Head>
+
+      {/* Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-6G9CLBBL99"
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-6G9CLBBL99', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+
       <Layout>
         <Component {...pageProps} />
       </Layout>
